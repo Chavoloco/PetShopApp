da89f90db008bd55a791354687a608f70e795d42
Chavoloco <58275860+Chavoloco@users.noreply.github.com>
2021-06-07 17:28:52 -0300

    aa

--- js/pets.js
+++ js/pets.js
@@ -1,1 +1,23 @@
-console.log("pets");
+let pets=[
+    {
+        type:"Gat",
+        name:"Garfield",
+        owner:"Raul"
+    }
+];
+
+
+function listPets(){
+    let htmlPets= pets.map((pet, index)=>`<tr>
+    <th scope="row">${index}</th>
+    <td>${pet.type}</td>
+    <td>${pet.name}</td>
+    <td>${pet.owner}</td>
+    <td>
+        <div class="btn-group" role="group" aria-label="Basic example">
+            <button type="button" class="btn btn-info"><i class="fas fa-edit"></i></button>
+            <button type="button" class="btn btn-danger"><i class="fas fa-trash"></i></button>
+        </div>
+    </td>
+</tr>`);
+}
