ffae73b5605d5679fa41bbdd14350735cd7cb9c2
Chavoloco <58275860+Chavoloco@users.noreply.github.com>
2021-06-08 00:36:35 -0300

    crud pets

--- js/pets.js
+++ js/pets.js
@@ -1,23 +1,97 @@
+const type=document.getElementById("type");
+const name=document.getElementById("name");
+const owner=document.getElementById("owner");
+const form=document.getElementById("form");
+const btnSave=document.getElementById("btnSave");
+const indice=document.getElementById("indice");
+const btnClose=document.getElementById("btnClose");
+const btnX=document.getElementById("btnX");
+
+const listPets=document.getElementById("list-pets");
+
 let pets=[
     {
-        type:"Gat",
-        name:"Garfield",
-        owner:"Raul"
+        type:"Cat",
+        nombre:"Garfield",
+        owner:"Lyman"
+    },
+    {
+        type:"Dog",
+        nombre:"Pluto",
+        owner:"Mickey"
     }
 ];
 
 
-function listPets(){
-    let htmlPets= pets.map((pet, index)=>`<tr>
-    <th scope="row">${index}</th>
-    <td>${pet.type}</td>
-    <td>${pet.name}</td>
-    <td>${pet.owner}</td>
-    <td>
-        <div class="btn-group" role="group" aria-label="Basic example">
-            <button type="button" class="btn btn-info"><i class="fas fa-edit"></i></button>
-            <button type="button" class="btn btn-danger"><i class="fas fa-trash"></i></button>
-        </div>
-    </td>
-</tr>`);
+function toListPets(){
+    const htmlPets= pets.map((pet, index)=>`<tr>
+            <th scope="row">${index}</th>
+            <td>${pet.type}</td>
+            <td>${pet.nombre}</td>
+            <td>${pet.owner}</td>
+            <td>
+            <div class="btn-group" role="group" aria-label="Basic example">
+            <button type="button" class="btn btn-info edit"  data-toggle="modal" data-target="#exampleModalCenter"><i class="fas fa-edit"></i></button>
+            <button type="button" class="btn btn-danger delete"><i class="fas fa-trash"></i></button>
+            </div>
+            </td>
+            </tr>`).join("");
+    listPets.innerHTML=htmlPets;
+    Array.from(document.getElementsByClassName("edit")).forEach((editButton,index)=>editButton.onclick=edit(index));
+    Array.from(document.getElementsByClassName("delete")).forEach((deleteButton,index)=>deleteButton.onclick=deleteData(index));
 }
+
+function submitData(event){
+    event.preventDefault();
+    const data ={
+        type: type.value,
+        nombre: nombre.value,
+        owner: owner.value
+    };
+    const action=btnSave.innerHTML;
+    switch(action){
+        case 'Edit':
+            //edit
+            pets[indice.value] = data;  
+            break;
+        default:
+            //create
+            pets.push(data);
+            break;
+    }
+    toListPets();
+    resetModal();
+}
+
+function edit(index){
+    return function ClickIt(){
+        btnSave.innerHTML='Edit';
+        $('#exampleModalCenter').modal('toggle');
+        const pet = pets[index];
+        nombre.value=pet.nombre;
+        owner.value=pet.owner;
+        type.value=pet.type;
+        indice.value=index;
+    }
+}
+
+function deleteData(index){
+    return function clickDelete(){
+        pets=pets.filter((pet,indexPet)=>indexPet !== index);//Este metodo filtra todos los elementos del array y devuelve los elementos que no filtro
+        toListPets();
+
+    }
+}
+
+function resetModal(){
+    nombre.value='';
+    owner.value='';
+    type.value='';
+    indice.value='';
+    btnSave.innerHTML='Create';
+}
+toListPets();
+form.onsubmit = submitData;
+btnSave.onclick=submitData;
+btnClose.onclick=resetModal;
+btnX.onclick=resetModal;
